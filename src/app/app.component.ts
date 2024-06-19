26535b35e7b578becac7a5171161e4f67440a846
daya-pao <dayana06052002@gmail.com>
2024-06-19 18:13:24 -0500

    nuevos estilos y maquetado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NavbarComponent],
+  imports: [RouterOutlet,HomeComponent,NavbarComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
