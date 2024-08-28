710be04edc3c6d8440cbdecee16b5316c2f19648
daya-pao <dayana06052002@gmail.com>
2024-08-27 19:09:52 -0500

    cambio de diseño en el componente de contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { ServicioComponent } from './components/servicio/servicio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NavbarComponent,AboutComponent,ServicioComponent],
+  imports: [RouterOutlet,HomeComponent,NavbarComponent,AboutComponent,ServicioComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
